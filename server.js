21c93fbe0744fd6a4127bf2a7a86cc2a1a2fee4f
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2025-04-08 01:24:15 -0300

    XLSX lib add / json endpoint added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const XLSX = require('xlsx');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,6 +25,26 @@ app.get('/fetch-xls', async (req, res) => {
         res.status(500).json({ error: 'Error al descargar el archivo', details: error.message });
     }
 });
+
+// Ruta para importar datos desde el archivo XLS y convertirlo a JSON   
+app.get('/api/products/imported-from-xls', async (req, res) => {
+  try {
+    const url = 'https://rhcomercial.com.ar/lista/listadepreciosrh.xls';
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+    const workbook = XLSX.read(response.data, { type: 'buffer' });
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    // Parámetro range: 15 salta las primeras 15 filas
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { raw: true, range: 15 });
+
+    res.json(jsonData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al procesar el archivo', details: error.message });
+  }
+});
+
 
 // Iniciar el servidor
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
