15e318e4d9534afac296ca780a12a9fa6aceb753
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2025-04-17 00:06:44 -0300

    process sections

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const XLSX = require('xlsx');
+const { processSheetItems } = require('../services/parser.service');
+
+const getParsedProducts = async (req, res) => {
+  try {
+    const url = 'https://rhcomercial.com.ar/lista/listadepreciosrh.xls';
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+    const workbook = XLSX.read(response.data, { type: 'buffer' });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const sheetItems = XLSX.utils.sheet_to_json(worksheet, { raw: true, range: 15 });
+
+    const parsedSections = processSheetItems(sheetItems);
+    res.json(parsedSections);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al procesar el archivo', details: error.message });
+  }
+};
+
+module.exports = { getParsedProducts };
---
+++ src/services/parser.service.js
@@ -0,0 +1,64 @@
+const {
+  PRODUCT_SECTIONS_CORRECT_REGEX,
+  PRODUCT_SECTIONS_CORRECT_MAP,
+  SPECIAL_NAME_CASES
+} = require('../utils/constants');
+const { sectionsData } = require("../utils/sectionsData");
+
+const { getProductPrefix, getProductPrefix1word } = require('../utils/helpers').default;
+const { generateEAN13 } = require('./barcode.service');
+
+function processSheetItems(sheetItems) {
+  const sectionMap = new Map(sectionsData.map(section => [section.title[0], { ...section, products: [] }]));
+  const productMap = new Map();
+
+  for (const item of sheetItems) {
+    const { CODIGO, DESCRIPCIÓN, RUBRO, PRECIO } = item;
+    let section = sectionMap.get(RUBRO);
+    if (!section) continue;
+
+    const matches = [...DESCRIPCIÓN.matchAll(PRODUCT_SECTIONS_CORRECT_REGEX)];
+    if (matches.length > 0) {
+      const lastMatch = matches.at(-1)[0].toUpperCase();
+      const correctedSection = PRODUCT_SECTIONS_CORRECT_MAP.get(lastMatch);
+      if (correctedSection) section = sectionMap.get(correctedSection[0]);
+    }
+
+    if (!section) continue;
+
+    const specialCase = SPECIAL_NAME_CASES.some(word =>
+      DESCRIPCIÓN.toUpperCase().includes(word.toUpperCase())
+    );
+
+    const productName = specialCase
+      ? getProductPrefix1word(DESCRIPCIÓN)
+      : getProductPrefix(DESCRIPCIÓN);
+
+    if (!productMap.has(productName)) {
+      productMap.set(productName, {
+        name: productName,
+        image: '',
+        items: []
+      });
+      section.products.push(productMap.get(productName));
+    }
+
+    const product = productMap.get(productName);
+    const codeStr = String(CODIGO).padStart(9, '0');
+
+    const newItem = {
+      code: CODIGO,
+      description: DESCRIPCIÓN,
+      price: PRECIO,
+      barcode: generateEAN13(codeStr)
+    };
+
+    product.items.push(newItem);
+    product.items.sort((a, b) => a.code - b.code);
+  }
+  console.log('Productos procesados:', productMap.size);
+
+  return Array.from(sectionMap.values());
+}
+
+module.exports = { processSheetItems };
